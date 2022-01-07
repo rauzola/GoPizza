8902267f84191b9898359aca7b7f9ac88444cefa
Raul Sigoli <raul_sigoli@hotmail.com>
2022-01-06 22:40:04 -0300

    Instalação e configuração do Firebase

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
+import { ThemeProvider } from 'styled-components/native';
+
+import theme from './src/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -10,14 +12,13 @@ export default function App() {
     DMSerifDisplay_400Regular
   });
 
-  if (fontsLoaded) {
+  if (!fontsLoaded) {
     return <AppLoading />
   }
 
   return (
-    <View >
+    <ThemeProvider theme={theme}>
 
-    </View>
+    </ThemeProvider>
   );
 }
-
