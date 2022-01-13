35d2874204808d1935510d2bae4c10d54ed351ef
Raul Sigoli <raul_sigoli@hotmail.com>
2022-01-13 14:50:21 -0300

    funcionou depois de 3 dias (ops: nao ta logando to arrumando)

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { KeyboardAvoidingView, Platform } from 'react-native';
+
+import { useAuth } from '@hooks/auth'
 
 import brandImg from '@assets/brand.png';
 
@@ -15,6 +17,15 @@ import {
 } from './styles';
 
 export function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { signIn, isLogging } = useAuth();
+
+  function handleSignIn() {
+    signIn(email, password);
+  }
+
   return (
     <Container>
       <KeyboardAvoidingView
@@ -30,12 +41,14 @@ export function SignIn() {
             type="secondary"
             autoCorrect={false}
             autoCapitalize="none"
+            onChangeText={setEmail}
           />
 
           <Input
             placeholder="Senha"
             type="secondary"
             secureTextEntry
+            onChangeText={setPassword}
           />
 
           <ForgotPasswordButton>
@@ -47,7 +60,8 @@ export function SignIn() {
           <Button
             title="Entrar"
             type="secondary"
-
+            onPress={handleSignIn}
+            isLoading={isLogging}
           />
         </Content>
       </KeyboardAvoidingView>
